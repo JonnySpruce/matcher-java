27db94536996d63e45744314709777718a11f02b
Johnston97 <lukejohnston97@live.com>
2019-03-13 16:12:23 +0000

    Revert "fixed"
    This reverts commit b965698da61632310f0962dc900bd4081a0f40d0.

--- service-skeleton/src/main/java/com/scottlogic/training/orders/OrderDTO.java
+++ service-skeleton/src/main/java/com/scottlogic/training/orders/OrderDTO.java
@@ -1,94 +1,44 @@
 package com.scottlogic.training.orders;
 
-import java.util.Comparator;
-import java.util.Date;
-
 /*
  *  This is a Data transfer object which carries the message from client to server and vice-versa.
  *  It's used by the OrderModule to serialize and deserialize messages from and for the client.
  *
  *  Add/remove fields (and types) from here as necessary in order to match your message format.
  */
-public class OrderDTO implements Comparable<OrderDTO> {
+public class OrderDTO {
 
-    private String accNo;
-    private Boolean orderType;
-    private float quantity;
-    private float price;
-    private float quantityRemaining;
-    private Date time;
-
+    private String userName;
+    private String orderType;
 
     public OrderDTO() {
     }
 
-    public OrderDTO(String accNo,  float price, float quantity, Boolean orderType) {
+    public OrderDTO(String userName, String orderType) {
         super();
-        this.accNo = accNo;
+        this.userName = userName;
         this.orderType = orderType;
-        this.quantity = quantity;
-        this.price = price;
-        this.quantityRemaining = quantity;
-        this.time = new Date();
-
     }
 
-    public String getAccNo() {
-        return accNo;
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
-    public Boolean getOrderType() {
+    public String getOrderType() {
         return orderType;
     }
-
-    public float getQuantity() {
-        return quantity;
-    }
-
-    public float getQuantityRemaining() {
-        return quantityRemaining;
-    }
-
-    public void setQuantityRemaining(float i) {
-        if(i == 0){
-            quantityRemaining = 0;
-        }
-        quantityRemaining -= i;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public Date getTime() {
-        return time;
-    }
-
-    public int compareTo(OrderDTO order) {
-        if(!order.getOrderType()) {
-            if (this.getPrice() < order.getPrice()) return -1;
-            if (this.getPrice() > order.getPrice()) return 1;
-            if (this.getTime().before(order.getTime())) return 1;
-            if (this.getTime().after(order.getTime())) return -1;
-            return 0;
-        } else {
-            if (this.getPrice() < order.getPrice()) return 1;
-            if (this.getPrice() > order.getPrice()) return -1;
-            if (this.getTime().before(order.getTime())) return -1;
-            if (this.getTime().after(order.getTime())) return 1;
-            return 0;
-        }
+    public void setOrderType(String orderType) {
+        this.orderType = orderType;
     }
 
     @Override
     public String toString() {
         return "OrderDTO{" +
-                "accNo='" + accNo + '\'' +
+                "userName='" + userName + '\'' +
                 ", orderType='" + orderType + '\'' +
-                ", quantity='" + quantity + '\'' +
-                ", price='" + price + '\'' +
-                ", quantityRemaining='" + quantityRemaining + '\'' +
-
                 '}';
     }
 }
