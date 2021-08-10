25efc7dbb985cc404b45cbfbf797fcc1631343c2
Jonny Spruce <jspruce94@gmail.com>
2021-08-10 18:04:53 +0100

    able to connect to websocket!

--- service-skeleton/src/main/java/com/scottlogic/training/websocket/OrderWebsocketController.java
+++ service-skeleton/src/main/java/com/scottlogic/training/websocket/OrderWebsocketController.java
@@ -1,20 +1,20 @@
-//package com.scottlogic.training.websocket;
-//
-//import com.scottlogic.training.matcher.Matcher;
-//import com.scottlogic.training.matcher.Order;
-//import com.scottlogic.training.matcher.Trade;
-//import org.springframework.messaging.handler.annotation.MessageMapping;
-//import org.springframework.messaging.handler.annotation.SendTo;
-//import org.springframework.stereotype.Controller;
-//import java.util.List;
-//
-//@Controller
-//public class OrderWebsocketController {
-//    @MessageMapping("/placeOrder")
-//    @SendTo("/topic/trades")
-//    public List<Trade> greeting(Order order) throws Exception {
-//        System.out.println("Order received!");
-//
-//        return Matcher.receiveOrder(order);
-//    }
-//}
+package com.scottlogic.training.websocket;
+
+import com.scottlogic.training.matcher.Matcher;
+import com.scottlogic.training.matcher.Order;
+import com.scottlogic.training.matcher.Trade;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import java.util.List;
+
+@Controller
+public class OrderWebsocketController {
+    @MessageMapping("/placeOrder")
+    @SendTo("/topic/trades")
+    public List<Trade> greeting(Order order) throws Exception {
+        System.out.println("Order received!");
+
+        return Matcher.receiveOrder(order);
+    }
+}
